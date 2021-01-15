ea83d0f8cd910622bb7ce856284d4b25a5dcca3c
Nikita Zhavoronkov <larkgronick@gmail.com>
2021-01-15 19:47:06 +0300

    feat: add load/save and change theme features

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import {loadPeople, loadPlanets, loadFilms, loadSpecies, loadVehicles} from './l
 
 let category;
 let categorySelected = false;
+let currentPageInfo;
+
 const cardsField = document.querySelector('.card-body');
 const categories = document.querySelectorAll(`.nav-link`);
 const paginator = document.createElement('ul');
@@ -9,6 +11,22 @@ const introImage = document.querySelector('.starwars-demo');
 const loader = document.querySelector('.lds-facebook');
 const search = document.getElementById('search');
 const searchField = document.querySelector('.search-field');
+const save = document.getElementById('save');
+const load = document.getElementById('load');
+const themeSwitcher = document.getElementById('customSwitch1');
+
+
+window.onload = function loadSavedData() {
+    let theme = localStorage.getItem('theme');
+
+    if (theme === 'dark') {
+        themeSwitcher.checked = true;
+    } else {
+        themeSwitcher.checked = false;
+    }
+    changeTheme();
+
+}
 
 categories.forEach(el => {
     el.addEventListener('click', (e) => {
@@ -16,37 +34,33 @@ categories.forEach(el => {
             if(el.classList[2]){
             el.classList.remove('selected') 
         }});
+
         e.target.classList.add('selected');
         category = el.innerHTML;
+
         loadCategory(el.innerHTML, 1);
+
     });
 })
 
-function getSearchData(category, value) {
-    cardsField.innerHTML = '';
-    fetch(`https://swapi.dev/api/${category.toLowerCase()}/?search=${value}`)
-    .then(res => res.json())
-    .then(data => {
-        if (data.results.length === 0) {
-            showMessage(`Don’t blame me. I’m an interpreter. I’m not supposed to know every ${category.toLowerCase()} in the Galaxy .`)
-        }
-       
-        generateCards(data.results, category)
-    })
-    .catch(err => console.log(err));
-}
-
+paginator.addEventListener('click', moveToPage);
+search.addEventListener('click', searchValue);
+save.addEventListener('click', saveData);
+load.addEventListener('click', loadData);
+themeSwitcher.addEventListener('click', changeTheme);
 
 function loadCategory(category, page) {
     cardsField.innerHTML = '';
     introImage.style.display = 'none';
     loader.style.display = 'inline-block';
     categorySelected = true;
+
     fetch(`https://www.swapi.tech/api/${category}?page=${page}&limit=10`)
         .then(res => res.json())
         .then(data => loadDetails(data, category, page))
         .then(() => loader.style.display = 'none')
         .catch(err => notifyByError(err.status));
+
 }
 
 function loadDetails(data, category, page){
@@ -54,22 +68,26 @@ function loadDetails(data, category, page){
         let pages = data.total_pages;
         let urls = data.results.map(el => el.url);
         let requests = urls.map(url => fetch(url));
+
         Promise.all(requests)
         .then(responses => Promise.all(responses.map(r => r.json())))
         .then(items => {
             let info = items.map(item => item.result.properties);
+
             generateCards(info, category);
             generatePagination(pages, page);
         })
         .catch(err => notifyByError(err))
+
     } else {
         let info = data.results.map(item => item.properties);
-        console.log(info);
+
         generateCards(info, category);
     }
 }
 
-function generateCards(info, category) {
+function generateCards(info, category) {  
+        currentPageInfo = info;
         let cardsContainer = document.createElement('div');
         cardsContainer.className = 'card-columns';
         cardsField.appendChild(cardsContainer);
@@ -77,6 +95,9 @@ function generateCards(info, category) {
     for (let i = 0; i < info.length; i++) {
         let card = document.createElement('div');
         card.className = 'card cover';
+        if (themeSwitcher.checked) {
+            card.classList.add('dark-side');
+        }
         cardsContainer.appendChild(card);
 
         switch (category) {
@@ -105,7 +126,6 @@ function generateCards(info, category) {
 
 }
 
-
 function generatePagination(pages, page) { 
     paginator.innerHTML = '';
     paginator.className ='pagination';
@@ -116,21 +136,44 @@ function generatePagination(pages, page) {
         li.className ='page-item';
         let a = document.createElement('a');
         a.className = 'page-link';
+
+        if (themeSwitcher.checked) {
+            a.classList.add('dark-side');
+        }
+
         a.innerText = i;
+
         paginator.appendChild(li);
         li.appendChild(a);
+
         if(i == page){
             li.classList.add('active');
         } 
     }
 }
 
-paginator.addEventListener('click', moveToPage)
+
+function getSearchData(category, value) {
+    cardsField.innerHTML = '';
+    fetch(`https://swapi.dev/api/${category.toLowerCase()}/?search=${value}`)
+    .then(res => res.json())
+    .then(data => {
+        if (data.results.length === 0) {
+            showMessage(`Don’t blame me. I’m an interpreter. I’m not supposed to know every ${category.toLowerCase()} in the Galaxy .`)
+        }
+       
+        generateCards(data.results, category);
+
+    })
+    .catch(err => console.log(err));
+
+}
+
 
 function moveToPage(e){
     let userChoice = e.target.innerText;
     window.scrollTo(0, 0);
-    loadCategory(category, userChoice)
+    loadCategory(category, userChoice);
     
 }
 
@@ -140,19 +183,16 @@ function notifyByError(err) {
 
 }
 
-search.addEventListener('click', searchValue);
-
 function searchValue(e) {
     e.preventDefault();
     let input = searchField.value;
-    console.log(input)
+
     if (!categorySelected) {
         showMessage('Please, select category first');
     } else {
-      
         getSearchData(category, input);
-       
     }
+
 }
 
 function showMessage(text) {
@@ -162,5 +202,58 @@ function showMessage(text) {
         message.className = 'message display-4';
         message.innerText = text;
         cardsField.appendChild(message);
+
+}
+
+function saveData() {
+    localStorage.setItem('info', JSON.stringify(currentPageInfo));
+    localStorage.setItem('category', category);
+    
+    if (themeSwitcher.checked) {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'white');
+    }
+
+    alert('Saved!')
+
+}
+
+function loadData() {
+    cardsField.innerHTML = '';
+    introImage.style.display = 'none';
+
+    let info = JSON.parse(localStorage.getItem('info'));
+    let category = localStorage.getItem('category');
+
+    generateCards(info, category);
+
+}
+
+function changeTheme(){
+    let navItems = Array.from(document.querySelectorAll('.nav-link'));    
+    let cards = Array.from(document.querySelectorAll('.cover'));   
+    let pages =  Array.from(document.querySelectorAll('.page-link'));    
+    let elements = navItems.concat(cards).concat(pages);
+
+    if(themeSwitcher.checked){
+        for (let i = 0; i < elements.length; i++) {
+            elements[i].classList.remove('white-side');   
+            elements[i].classList.add('dark-side');   
+        }
+        
+        searchField.classList.add('dark-side');
+        searchField.classList.remove('white-side');
+        themeSwitcher.nextElementSibling.textContent = 'Dark Side';
+    } else {
+        for (let i = 0; i < elements.length; i++) {
+            elements[i].classList.remove('dark-side');   
+            elements[i].classList.add('white-side');   
+        }
+        searchField.classList.add('white-side');
+        searchField.classList.remove('dark-side');
+        themeSwitcher.nextElementSibling.textContent = 'White Side';
+
+    }
 
 }
